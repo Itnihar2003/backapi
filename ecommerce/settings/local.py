43a8f2dfd588bd06b544824e3cb8fca10284c136
Nihar <beheraniharranjan2003@gmail.com>
2025-04-05 02:38:21 +0530

    postgray

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -1,5 +1,6 @@
 from .base import *
 
 DATABASES = {
-    'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL","postgresql://testdb_4qio_user:dyMZeIr3w5cpywknfyPE6yHLjK4pydN7@dpg-cvo33l49c44c73bhvg40-a.oregon-postgres.render.com/testdb_4qio"))
 }
+ 
